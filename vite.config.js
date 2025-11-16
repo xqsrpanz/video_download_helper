15bef54cbd8197f1b34f9a45896bcc64f6ef4768
PANZ <xqsrpanz@gmail.com>
2025-11-17 01:15:41 +0800

    refactor: 重新组织文件结构

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,14 @@
 import { defineConfig } from 'vite';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  publicDir: false,
+  publicDir: 'public',
+  copyPublicDir: true,
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        background: 'src/index.js',
-      },
-      output: {
-        entryFileNames: 'background/index.js',
-      },
-      plugins: [
-        copy({
-          hook: 'writeBundle',
-          targets: [
-            { src: 'manifest.json', dest: 'dist' },
-            { src: 'assets', dest: 'dist' },
-            { src: 'content_scripts', dest: 'dist' },
-            { src: 'popup', dest: 'dist' },
-            { src: 'options', dest: 'dist' },
-          ],
-        }),
-      ],
+      input: { background: 'src/index.js' },
+      output: { entryFileNames: 'background/index.js' },
     },
   },
 });
