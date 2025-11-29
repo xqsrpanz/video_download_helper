caa5ee28e3aaed79c7b9c068141a74280a7db44b
PANZ <xqsrpanz@gmail.com>
2025-11-29 16:46:31 +0800

    feat: add default css and king-design

--- popup/App.vue
+++ popup/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>这是 Popup Vue 页面！ {{ count }}</div>
+  <div class="popup-container">这是 Popup Vue 页面！ {{ count }}</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const count = ref(0)
 </script>
+
+<style scoped>
+.popup-container {
+  width: 200px;
+  height: 300px;
+  background-color: #fff;
+}
+</style>
